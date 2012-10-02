ee4617978dab4cdad761db58dfad099dc427d929
Kyle McElyea <kyle.mcelyea@hobbylobby.com>
2012-10-02 13:54:35 -0500

    Added Bar Clock
    Mostly because it is just kinda fun and cool to do,but added a bar clock that is hidden till the user shows it using the status bar menu. Ticks up current seconds mins and hours into bars in real time. Added position configurations for it too

---
+++ TimeWarp/Views/KMBarClockViewController.h
@@ -0,0 +1,13 @@
+//
+//  KMBarClockViewController.h
+//  TimeWarp
+//
+//  Created by Kyle J. McElyea on 10/2/12.
+//  Copyright (c) 2012 Kyle J. McElyea. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KMBarClockViewController : NSViewController
+
+@end
---
+++ TimeWarp/Views/KMBarClockViewController.m
@@ -0,0 +1,27 @@
+//
+//  KMBarClockViewController.m
+//  TimeWarp
+//
+//  Created by Kyle J. McElyea on 10/2/12.
+//  Copyright (c) 2012 Kyle J. McElyea. All rights reserved.
+//
+
+#import "KMBarClockViewController.h"
+
+@interface KMBarClockViewController ()
+
+@end
+
+@implementation KMBarClockViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
